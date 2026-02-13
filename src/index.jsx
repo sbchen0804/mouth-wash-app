5df000444c789ce2bbf5516d30a7ae03d0e63b25
sbchen0804 <sbchen0804@gmail.com>
2026-02-13 21:35:11 +0800

    Refactor index.jsx to use React.StrictMode

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,13 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-
-import App from './components/App.jsx';
-
 import './index.css';
+import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
